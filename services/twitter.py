e18a6ba4336bc22d94e9dc0dfb6274beb686e4f3
human058382928 <162091348+human058382928@users.noreply.github.com>
2025-02-11 21:34:47 -0800

    update

--- services/twitter.py
+++ services/twitter.py
@@ -208,6 +208,20 @@ class TwitterMentionHandler:
             access_secret=config.access_secret,
         )
 
+    async def _post_response(
+        self, tweet_data: TweetData, response_content: str
+    ) -> None:
+        """Post a response tweet.
+
+        Args:
+            tweet_data: Data about the tweet to respond to
+            response_content: Content of the response tweet
+        """
+        logger.debug(f"Posting response to tweet {tweet_data.tweet_id}")
+        await self.twitter_service._apost_tweet(
+            text=response_content, reply_in_reply_to_tweet_id=tweet_data.tweet_id
+        )
+
     def _is_author_whitelisted(self, author_id: str) -> bool:
         """Check if the author is in the whitelist."""
         logger.debug(f"Checking whitelist status for author {author_id}")
